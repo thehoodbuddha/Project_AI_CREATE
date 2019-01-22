a8490d27f7058b81ea4b93ffdcfc61d2b687eb99
jan-paul konijn <jan-paul2012@live.nl>
2019-01-22 10:55:57 +0100

    mainloop change

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,9 +4,7 @@
 
 #include "ofApp.h"
 #include <thread>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
+
 //--------------------------------------------------------------
 GridElement* pacStartLocation;
 
